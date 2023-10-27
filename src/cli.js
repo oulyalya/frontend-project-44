38864da55cbf1a965cf143876d4032791934a8d2
Olga <olya.debelaya@gmail.com>
2023-10-27 19:13:51 +0300

    fix linting errors

--- src/cli.js
+++ src/cli.js
@@ -3,6 +3,6 @@ import readlineSync from 'readline-sync';
 export const name = readlineSync.question('May I have your name? ');
 
 export default () => {
-    console.log('Welcome to the Brain Games!');
-    console.log(`Hello, ${name}!`);
+  console.log('Welcome to the Brain Games!');
+  console.log(`Hello, ${name}!`);
 };
