08f864ac7300f284ae01b3435aeadd92333a6e5c
Olga <olya.debelaya@gmail.com>
2023-11-02 22:05:12 +0300

    fix game rounds counter

--- src/gameStages/playGameRound.js
+++ src/gameStages/playGameRound.js
@@ -1,17 +1,10 @@
 import readlineSync from 'readline-sync';
-import { name } from '../cli.js';
-import { MAX_GAME_ROUNDS } from '../consts.js';
 import processAnswer from './processAnswer.js';
 
 const playGameRound = (question, expectedAnswer, currentRound) => {
   console.log(question);
   const answer = readlineSync.question('Your answer: ');
-
-  const result = processAnswer(answer, expectedAnswer);
-
-  if (currentRound === MAX_GAME_ROUNDS) {
-    console.log(`Congratulations, ${name}!`);
-  }
+  const result = processAnswer(answer, expectedAnswer, currentRound);
 
   return result;
 };
--- src/gameStages/processAnswer.js
+++ src/gameStages/processAnswer.js
@@ -1,10 +1,15 @@
 import { name } from '../cli.js';
+import { MAX_GAME_ROUNDS } from '../consts.js';
 
-const processAnswer = (answer, expectedAnswer) => {
+const processAnswer = (answer, expectedAnswer, currentRound) => {
   const isCorrect = answer.toLowerCase() === expectedAnswer.toLowerCase();
 
   if (isCorrect) {
     console.log('Correct!');
+
+    if (currentRound + 1 === MAX_GAME_ROUNDS) {
+      console.log(`Congratulations, ${name}!`);
+    }
   } else {
     console.log(`'${answer}' is wrong answer ;(. Correct answer was '${expectedAnswer}'.`);
     console.log(`Let's try again, ${name}!`);
